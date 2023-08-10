57c9a8a24472fe89aec969ba3c7ff797513e0367
tgpriya <141907697+tgpriya@users.noreply.github.com>
2023-08-10 13:14:07 +0530

    Add files via upload

---
+++ Java/Activities/Activity_01.java
@@ -0,0 +1,18 @@
+package Activites;
+
+public class Activity_01 {
+    public static void main(String[] args) {
+        Car maruthi = new Car();
+        maruthi.make= 2019;
+        maruthi.color="White";
+        maruthi.transmission="manual";
+
+        maruthi.displayCharacterstics();
+        maruthi.accelerate();
+        maruthi.brake();
+
+
+    }
+
+
+}
